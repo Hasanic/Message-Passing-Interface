27d0c148aa09bddc898e19401a6162aa8ee43e38
Hasanic <acgism@ZePengs-MacBook-Pro.local>
2016-11-10 02:13:37 +0800

    UP

---
+++ simulation.c
@@ -0,0 +1,311 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "datadef.h"
+#include "init.h"
+#include "mpi.h"
+
+#define max(x,y) ((x)>(y)?(x):(y))
+#define min(x,y) ((x)<(y)?(x):(y))
+
+extern int *ileft, *iright;
+extern int nprocs, proc;
+
+/* Computation of tentative velocity field (f, g) */
+void compute_tentative_velocity(float **u, float **v, float **f, float **g,
+    char **flag, int imax, int jmax, float del_t, float delx, float dely,
+    float gamma, float Re)
+{
+    int  i, j;
+    float du2dx, duvdy, duvdx, dv2dy, laplu, laplv;
+
+    for (i=1; i<=imax-1; i++) {
+        for (j=1; j<=jmax; j++) {
+            /* only if both adjacent cells are fluid cells */
+            if ((flag[i][j] & C_F) && (flag[i+1][j] & C_F)) {
+                du2dx = ((u[i][j]+u[i+1][j])*(u[i][j]+u[i+1][j])+
+                    gamma*fabs(u[i][j]+u[i+1][j])*(u[i][j]-u[i+1][j])-
+                    (u[i-1][j]+u[i][j])*(u[i-1][j]+u[i][j])-
+                    gamma*fabs(u[i-1][j]+u[i][j])*(u[i-1][j]-u[i][j]))
+                    /(4.0*delx);
+                duvdy = ((v[i][j]+v[i+1][j])*(u[i][j]+u[i][j+1])+
+                    gamma*fabs(v[i][j]+v[i+1][j])*(u[i][j]-u[i][j+1])-
+                    (v[i][j-1]+v[i+1][j-1])*(u[i][j-1]+u[i][j])-
+                    gamma*fabs(v[i][j-1]+v[i+1][j-1])*(u[i][j-1]-u[i][j]))
+                    /(4.0*dely);
+                laplu = (u[i+1][j]-2.0*u[i][j]+u[i-1][j])/delx/delx+
+                    (u[i][j+1]-2.0*u[i][j]+u[i][j-1])/dely/dely;
+
+                f[i][j] = u[i][j]+del_t*(laplu/Re-du2dx-duvdy);
+            } else {
+                f[i][j] = u[i][j];
+            }
+        }
+    }
+
+    for (i=1; i<=imax; i++) {
+        for (j=1; j<=jmax-1; j++) {
+            /* only if both adjacent cells are fluid cells */
+            if ((flag[i][j] & C_F) && (flag[i][j+1] & C_F)) {
+                duvdx = ((u[i][j]+u[i][j+1])*(v[i][j]+v[i+1][j])+
+                    gamma*fabs(u[i][j]+u[i][j+1])*(v[i][j]-v[i+1][j])-
+                    (u[i-1][j]+u[i-1][j+1])*(v[i-1][j]+v[i][j])-
+                    gamma*fabs(u[i-1][j]+u[i-1][j+1])*(v[i-1][j]-v[i][j]))
+                    /(4.0*delx);
+                dv2dy = ((v[i][j]+v[i][j+1])*(v[i][j]+v[i][j+1])+
+                    gamma*fabs(v[i][j]+v[i][j+1])*(v[i][j]-v[i][j+1])-
+                    (v[i][j-1]+v[i][j])*(v[i][j-1]+v[i][j])-
+                    gamma*fabs(v[i][j-1]+v[i][j])*(v[i][j-1]-v[i][j]))
+                    /(4.0*dely);
+
+                laplv = (v[i+1][j]-2.0*v[i][j]+v[i-1][j])/delx/delx+
+                    (v[i][j+1]-2.0*v[i][j]+v[i][j-1])/dely/dely;
+
+                g[i][j] = v[i][j]+del_t*(laplv/Re-duvdx-dv2dy);
+            } else {
+                g[i][j] = v[i][j];
+            }
+        }
+    }
+
+    /* f & g at external boundaries */
+    for (j=1; j<=jmax; j++) {
+        f[0][j]    = u[0][j];
+        f[imax][j] = u[imax][j];
+    }
+    for (i=1; i<=imax; i++) {
+        g[i][0]    = v[i][0];
+        g[i][jmax] = v[i][jmax];
+    }
+}
+
+
+/* Calculate the right hand side of the pressure equation */
+void compute_rhs(float **f, float **g, float **rhs, char **flag, int imax,
+    int jmax, float del_t, float delx, float dely)
+{
+    int i, j;
+
+    for (i=1;i<=imax;i++) {
+        for (j=1;j<=jmax;j++) {
+            if (flag[i][j] & C_F) {
+                /* only for fluid and non-surface cells */
+                rhs[i][j] = (
+                             (f[i][j]-f[i-1][j])/delx +
+                             (g[i][j]-g[i][j-1])/dely
+                            ) / del_t;
+            }
+        }
+    }
+}
+
+
+/* Red/Black SOR to solve the poisson equation */
+int poisson(float **p, float **rhs, char **flag, int imax, int jmax,
+    float delx, float dely, float eps, int itermax, float omega,
+    float *res, int ifull,
+	// add these
+	/* in  */ const int rank,
+	/* in  */ const int processCount,
+	/* in  */ const int sliceSize,
+	/* out */ double* ptransTime
+	)
+{
+    int i, j, iter;
+    float add, beta_2, beta_mod;
+    float p0 = 0.0;
+
+    int rb; /* Red-black value. */
+
+    float rdx2 = 1.0/(delx*delx);
+    float rdy2 = 1.0/(dely*dely);
+    beta_2 = -omega/(2.0*(rdx2+rdy2));
+
+	int n;
+	double timeTmp = 0;
+
+    /* Calculate sum of squares */
+	float* p0Tmp = (float*)calloc(processCount,sizeof(float)); /* helper array */
+	for(i = rank*sliceSize+1; i < (rank+1)*sliceSize+1; ++i) {
+        for (j=1; j<=jmax; j++) {
+            if (flag[i][j] & C_F) { p0Tmp[rank] += p[i][j]*p[i][j]; }
+        }
+    }
+	// merge p0Tmp array
+	timeTmp = MPI_Wtime();
+	MPI_Allgather(
+		// send
+		&(p0Tmp[rank]),
+		1,
+		MPI_FLOAT,
+		// p0v
+		&(p0Tmp[0]),
+		1,
+		MPI_FLOAT,
+		// to all
+		MPI_COMM_WORLD
+	);
+	*ptransTime += MPI_Wtime() - timeTmp;
+	// sum
+	for(n = 0; n < processCount; ++n)
+		p0 += p0Tmp[n];
+
+	free(p0Tmp);
+
+    p0 = sqrt(p0/ifull);
+    if (p0 < 0.0001) { p0 = 1.0; }
+
+    /* Red/Black SOR-iteration */
+    for (iter = 0; iter < itermax; iter++) {
+        for (rb = 0; rb <= 1; rb++) {
+            for(i = rank*sliceSize+1; i < (rank+1)*sliceSize+1; ++i) {
+                for (j = 1; j <= jmax; j++) {
+                    if ((i+j) % 2 != rb) { continue; }
+                    if (flag[i][j] == (C_F | B_NSEW)) {
+                        /* five point star for interior fluid cells */
+                        p[i][j] = (1.-omega)*p[i][j] -
+                              beta_2*(
+                                    (p[i+1][j]+p[i-1][j])*rdx2
+                                  + (p[i][j+1]+p[i][j-1])*rdy2
+                                  -  rhs[i][j]
+                              );
+                    } else if (flag[i][j] & C_F) {
+                        /* modified star near boundary */
+                        beta_mod = -omega/((eps_E+eps_W)*rdx2+(eps_N+eps_S)*rdy2);
+                        p[i][j] = (1.-omega)*p[i][j] -
+                            beta_mod*(
+                                  (eps_E*p[i+1][j]+eps_W*p[i-1][j])*rdx2
+                                + (eps_N*p[i][j+1]+eps_S*p[i][j-1])*rdy2
+                                - rhs[i][j]
+                            );
+                    }
+                } /* end of j */
+            }  /* for(i = rank ... ) */
+			// merge p
+			timeTmp = MPI_Wtime();
+			MPI_Allgather(
+				// send
+				&(p[rank*sliceSize+1][0]),
+				(jmax+2)*sliceSize,
+				MPI_FLOAT,
+				// resv
+				&(p[1][0]),
+				(jmax+2)*sliceSize,
+				MPI_FLOAT,
+				// to all
+				MPI_COMM_WORLD
+			);
+			*ptransTime += MPI_Wtime() - timeTmp;
+
+        } /* end of rb */
+
+        /* Partial computation of residual */
+        *res = 0.0;
+		float* resTmp = (float*)calloc(processCount,sizeof(float)); /* helper array */
+		for(i = rank*sliceSize+1; i < (rank+1)*sliceSize+1; ++i) {
+            for (j = 1; j <= jmax; j++) {
+                if (flag[i][j] & C_F) {
+                    /* only fluid cells */
+                    add = (eps_E*(p[i+1][j]-p[i][j]) -
+                        eps_W*(p[i][j]-p[i-1][j])) * rdx2  +
+                        (eps_N*(p[i][j+1]-p[i][j]) -
+                        eps_S*(p[i][j]-p[i][j-1])) * rdy2  -  rhs[i][j];
+                    resTmp[rank] += add*add; /* save it here */
+                }
+            }
+        }
+		// merge resTmp array
+		timeTmp = MPI_Wtime();
+		MPI_Allgather(
+			// send
+			&(resTmp[rank]),
+			1,
+			MPI_FLOAT,
+			// resv
+			&(resTmp[0]),
+			1,
+			MPI_FLOAT,
+			// to all
+			MPI_COMM_WORLD
+		);
+		*ptransTime += MPI_Wtime() - timeTmp;
+		// sum
+		for(n = 0; n < processCount; ++n)
+			*res += resTmp[n];
+
+		free(resTmp);
+
+        *res = sqrt((*res)/ifull)/p0;
+
+        /* convergence? */
+        if (*res<eps) break;
+    } /* end of iter */
+
+    return iter;
+}
+
+
+/* Update the velocity values based on the tentative
+ * velocity values and the new pressure matrix
+ */
+void update_velocity(float **u, float **v, float **f, float **g, float **p,
+    char **flag, int imax, int jmax, float del_t, float delx, float dely)
+{
+    int i, j;
+
+    for (i=1; i<=imax-1; i++) {
+        for (j=1; j<=jmax; j++) {
+            /* only if both adjacent cells are fluid cells */
+            if ((flag[i][j] & C_F) && (flag[i+1][j] & C_F)) {
+                u[i][j] = f[i][j]-(p[i+1][j]-p[i][j])*del_t/delx;
+            }
+        }
+    }
+    for (i=1; i<=imax; i++) {
+        for (j=1; j<=jmax-1; j++) {
+            /* only if both adjacent cells are fluid cells */
+            if ((flag[i][j] & C_F) && (flag[i][j+1] & C_F)) {
+                v[i][j] = g[i][j]-(p[i][j+1]-p[i][j])*del_t/dely;
+            }
+        }
+    }
+}
+
+
+/* Set the timestep size so that we satisfy the Courant-Friedrichs-Lewy
+ * conditions (ie no particle moves more than one cell width in one
+ * timestep). Otherwise the simulation becomes unstable.
+ */
+void set_timestep_interval(float *del_t, int imax, int jmax, float delx,
+    float dely, float **u, float **v, float Re, float tau)
+{
+    int i, j;
+    float umax, vmax, deltu, deltv, deltRe;
+
+    /* del_t satisfying CFL conditions */
+    if (tau >= 1.0e-10) { /* else no time stepsize control */
+        umax = 1.0e-10;
+        vmax = 1.0e-10;
+        for (i=0; i<=imax+1; i++) {
+            for (j=1; j<=jmax+1; j++) {
+                umax = max(fabs(u[i][j]), umax);
+            }
+        }
+        for (i=1; i<=imax+1; i++) {
+            for (j=0; j<=jmax+1; j++) {
+                vmax = max(fabs(v[i][j]), vmax);
+            }
+        }
+
+        deltu = delx/umax;
+        deltv = dely/vmax;
+        deltRe = 1/(1/(delx*delx)+1/(dely*dely))*Re/2.0;
+
+        if (deltu<deltv) {
+            *del_t = min(deltu, deltRe);
+        } else {
+            *del_t = min(deltv, deltRe);
+        }
+        *del_t = tau * (*del_t); /* multiply by safety factor */
+    }
+}
